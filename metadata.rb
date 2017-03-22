0dd47fb5b8f01daf425f24fe33038d89bb170d5c
Tim Smith <tsmith@chef.io>
2017-03-21 21:06:03 -0700

    Update Apache license string in metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'apache2-windows'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs and configures Apache on Microsoft Windows platforms.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
