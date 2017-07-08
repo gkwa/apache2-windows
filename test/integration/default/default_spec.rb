6cebbc12ca529f2c8bc24102ca0b9b4820ba0778
Taylor Monacelli <taylormonacelli@gmail.com>
2017-07-08 16:21:40 -0700

    Add integration test

---
+++ test/integration/default/default_spec.rb
@@ -0,0 +1,8 @@
+describe file('c:\Program Files (x86)\Apache Software Foundation\Apache2.2\bin\httpd.exe') do
+  it { should exist }
+end
+
+describe command('cmd /c "c:\Program Files (x86)\Apache Software Foundation\Apache2.2\bin\httpd.exe" -v') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match %r{Server version: Apache/2.2.25} }
+end
