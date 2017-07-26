2bf63cd97a871d94d34f3c998a68f51afdaa769b
Taylor Monacelli <taylormonacelli@gmail.com>
2017-07-26 01:19:44 -0700

    Method 1: php custom resource using FileEdit()

--- resources/apache2_config.rb
+++ resources/apache2_config.rb
@@ -9,12 +9,28 @@ property :variables, Hash, default: {}
 action :create do
   directory new_resource.conf_dir
 
-  template "#{new_resource.conf_dir}/#{new_resource.config_name}.conf" do
+  full_conf_path = "#{new_resource.conf_dir}/#{new_resource.config_name}.conf"
+
+  template "#{full_conf_path}" do
     variables(new_resource.variables)
     source new_resource.source
     cookbook new_resource.cookbook
     action :create
     notifies :create, "template[#{node['apache']['windows']['conf']}]", :immediately
+  end
+
+  # fixme: this pains me, there must be better way
+  # every chef-client run will re-generate httpd.conf since fileedit() updates
+  # httpd.conf then chef-client realizes file is different from template and updates
+  # conf, then fileedit() re-appends to httpd.conf
+  ruby_block "Append this resource conf to httpd.conf" do
+    block do
+      this_include = "Include '#{full_conf_path}'"
+      httpd_conf = node['apache']['windows']['conf']
+      fe = Chef::Util::FileEdit.new("#{httpd_conf}")
+      fe.insert_line_if_no_match(/"#{this_include}"/, "Include '#{full_conf_path}'")
+      fe.write_file
+    end
   end
 end
 
